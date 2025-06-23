bb89e9bac8544b5edcc92409e10908bdd2dfb91f
Rana Usman <usman4243ch@gmail.com>
2025-06-22 23:42:06 -0700

    update app name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import AppRoutes from './routes/layouts/AppRoutes';
 import useAutoLogout from './utils/setupAutoLogout';
 
 function App() {
+  
   useAutoLogout();
+
   return <AppRoutes />;
 }
 export default App;
